b1a54d938fa698478078b62a5f8624cd95766e5d
co1umbine <hukutunojyoni@gmail.com>
2022-09-25 00:08:47 +0900

    [update] schedules seeder

--- database/seeders/SchedulesTableSeeder.php
+++ database/seeders/SchedulesTableSeeder.php
@@ -15,13 +15,19 @@ class SchedulesTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
+            $date_time = new DateTime();
+            $date_time->setDate(2022,8, 29+$i*3)->setTime(rand(0, 23), rand(5, 59), 0);
+
+            $quote = new DateTime;
+            $quote->setTime(rand(0, 72), rand(0, 59), 0);
+
             Schedule::create([
                 'user_id' => 1,
                 'name' => 'name' . $i,
                 'color' => 'FFFFFF',
-                'start_on' => new DateTime(),
-                'quote' => new DateTime(),
+                'start_on' => $date_time,
+                'quote' => $quote,
                 'note' => 'note' . $i,
             ]);
         }
